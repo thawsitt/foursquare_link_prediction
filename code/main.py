1900dd4af0611fe210c2bba56b3c4580bac8d75a
thawsitt <thawsitt@cs.stanford.edu>
2018-12-08 21:58:37 -0800

    Validate results

--- code/main.py
+++ code/main.py
@@ -3,22 +3,43 @@ import heuristics
 import time
 
 def train(graph, users, venues, score_fn):
+    print('Calculating scores for the training set...')
     start = time.clock()
     scores = []
     for u in users:
         for v in venues:
+            # if graph.IsEdge(u, v):
+            #     continue
             score = score_fn(graph, u, v)
             scores.append(((u, v), score))
     print('Calculations complete! Time taken: {}s'.format(time.clock() - start))
     print('Top 10 most similar nodes')
-    for item in sorted(scores, key=lambda x: -x[1])[:10]:
+    scores.sort(key=lambda x: -x[1])
+    for item in scores[:10]:
         print(item)
+    return scores
+
+def validate(test_graph, scores):
+    TP = 0
+    FP = 0
+    cutoff = len(scores)
+    for node_pair, score in scores[:cutoff]:
+        u, v = node_pair
+        if test_graph.IsEdge(u, v):
+            TP += 1
+        else:
+            FP += 1
+    print('# TP: {}'.format(TP))
+    print('# FP: {}'.format(FP))
+    print('Accuracy: {}%'.format(TP * 100.0 / test_graph.GetEdges()))
 
 def main():
-    graph = load_graph('../data/processed/sampled_checkins.txt')
-    users, venues = split_user_venues(graph)
-    score_fn = heuristics.num_common_neighbors_venue
-    train(graph, users, venues, score_fn)
+    SCORE_FN = heuristics.num_common_neighbors_venue
+    training_graph = load_graph('../data/training/train.txt')
+    test_graph = load_graph('../data/test/test.txt')
+    users, venues = split_user_venues(training_graph)
+    scores = train(training_graph, users, venues, SCORE_FN)
+    validate(test_graph, scores)
 
 #*******************************************************************************
 # Helper functions
